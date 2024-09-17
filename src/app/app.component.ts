cb44cefd0056573ec4eb91fde523723674f5e7fa
witoldolszyk <witoldolszyk@gmail.com>
2024-09-17 17:45:58 +0200

    Footer & Header -added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { PeriodicTableComponent } from './components/periodic-table/periodic-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PeriodicTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/periodic-table/periodic-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-periodic-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './periodic-table.component.html',
+  styleUrl: './periodic-table.component.css'
+})
+export class PeriodicTableComponent {
+
+}
